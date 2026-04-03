93cc85fb78490c589e28280826da28107be7e9c6
Ricardo Cardoso <ricardomanolo90@gmail.com>
2026-04-03 17:30:42 -0300

    udpate commit

--- src/utils/cards-infos.js
+++ src/utils/cards-infos.js
@@ -2,6 +2,12 @@ import CalcIMC from '../images/react/calc-imc.png';
 import DevMemory from '../images/react/dev-memory.png';
 import DogSocialNetwork from '../images/react/dog-social-network.png';
 import DTMoney from '../images/react/dt-money.png';
+import EventPlatform from '../images/react/event-platform.png';
+import GuessWhat from '../images/react/guess-what.png';
+import IgniteFeed from '../images/react/ignite-feed.png';
+import IgniteTimer from '../images/react/ignite-timer.png';
+import RocketNotes from '../images/react/rocket-notes.png';
+import TokenLab from '../images/react/token-lab.png';
 
 export const CardsInfos = [
   //AQUI VOU COLOCAR OS PROJETOS FEITOS EM REACT.
@@ -36,5 +42,53 @@ export const CardsInfos = [
     image: DTMoney,
     category: 'React',
     description: 'O DT Money é uma aplicação web desenvolvida com React e TypeScript que permite o controle de finanças pessoais de forma simples e eficiente...',
+  },
+
+  {
+    link: 'https://github.com/ricardocardoso90/react-project-event-platform',
+    title: 'Event Platform',
+    image: EventPlatform,
+    category: 'React',
+    description: 'O Event Platform, uma aplicação web construída com React e TypeScript, que simula o agendamento e a exibição de aulas...',
+  },
+
+  {
+    link: 'https://github.com/ricardocardoso90/react-project-guess-what',
+    title: 'Guess What',
+    image: GuessWhat,
+    category: 'React',
+    description: 'O “Adivinhe” é um jogo interativo de adivinhação de palavras, onde o jogador precisa descobrir a palavra correta a partir de uma dica exibida na tela...',
+  },
+
+  {
+    link: 'https://github.com/ricardocardoso90/react-project-ignite-feed',
+    title: 'Ignite Feed',
+    image: IgniteFeed,
+    category: 'React',
+    description: 'O Ignite Feed é uma aplicação web criada com React e TypeScript, que simula o feed de uma rede social. Este projeto tem como objetivo reforçar conceitos fundamentais do React...',
+  },
+
+  {
+    link: 'https://github.com/ricardocardoso90/react-project-ignite-timer',
+    title: 'Ignite Timer',
+    image: IgniteTimer,
+    category: 'React',
+    description: 'O Ignite Timer é uma aplicação web desenvolvida com React e TypeScript, criada para ajudar o usuário a gerenciar suas tarefas utilizando a técnica Pomodoro. Com ele, é possível...',
+  },
+
+  {
+    link: 'https://github.com/ricardocardoso90/react-project-rocket-notes',
+    title: 'Rocket Notes',
+    image: RocketNotes,
+    category: 'React',
+    description: 'Aplicativo web de gerenciamento de notas desenvolvido com React, TypeScript e Styled Components, focado em organizar e editar conteúdo de forma intuitiva...',
+  },
+
+  {
+    link: 'https://github.com/ricardocardoso90/react-project-token-lab',
+    title: 'Token Lab',
+    image: TokenLab,
+    category: 'React',
+    description: 'Projeto front‑end desenvolvido com React, focado na utilização de formulários e gerenciamento de entradas de dados com React Hook Form e Yup para validação...',
   },
 ];
